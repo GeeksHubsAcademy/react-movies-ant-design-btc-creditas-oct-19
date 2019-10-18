dfa6dd53bcad0fddbbc20b018e495e484dc363af
jupegarnica <jupegarnica@gmail.com>
2019-10-18 12:09:13 +0200

    loading working

--- src/views/MoviesCategory.jsx
+++ src/views/MoviesCategory.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import apiService from '../services/moviesApi.service';
+import { Spin } from 'antd';
 class MoviesCategory extends React.Component {
   state = {
     movies: [],
     currentCategory: '',
+    loading: false,
   };
   constructor(p) {
     super(p);
     this.componentDidMount = this.getMovies;
-    this.componentDidUpdate = this.getMovies
+    this.componentDidUpdate = this.getMovies;
   }
 
-
-  getMovies() {
+  async getMovies() {
     const currentCategory = this.props.match.params.categoryName;
-    if (currentCategory !== this.state.currentCategory) {
-      apiService.getMoviesByCategory(currentCategory).then(data => {
-        this.setState({ movies: data.results, currentCategory });
-      });
+    if (currentCategory !== this.state.currentCategory && !this.state.loading) {
+      this.setState({ loading: true });
+      try {
+        const data = await apiService.getMoviesByCategory(currentCategory);
+        this.setState({
+          movies: data.results,
+          currentCategory,
+          loading: false,
+        });
+      } catch {
+        this.setState({ loading: false });
+      }
     }
   }
 
@@ -26,7 +35,13 @@ class MoviesCategory extends React.Component {
 
     return (
       <div className='MoviesCategory'>
-        <pre>{JSON.stringify(this.state.movies, null, 2)}</pre>
+        {this.state.loading ? (
+          <div style={{display:'flex', justifyContent:'center',padding:'2em'}}>
+            <Spin size='large' />
+          </div>
+        ) : (
+          <pre>{JSON.stringify(this.state.movies, null, 2)}</pre>
+        )}
       </div>
     );
   }

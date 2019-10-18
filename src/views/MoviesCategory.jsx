a7485f45ccce6a852378202c383b54e3a2c0c458
jupegarnica <jupegarnica@gmail.com>
2019-10-18 12:18:35 +0200

    loading and error feedback working

--- src/views/MoviesCategory.jsx
+++ src/views/MoviesCategory.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import apiService from '../services/moviesApi.service';
-import { Spin } from 'antd';
+import { Spin, Alert } from 'antd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 1em;
+`;
+
 class MoviesCategory extends React.Component {
   state = {
     movies: [],
     currentCategory: '',
     loading: false,
+    error: false,
   };
   constructor(p) {
     super(p);
@@ -16,16 +23,21 @@ class MoviesCategory extends React.Component {
   async getMovies() {
     const currentCategory = this.props.match.params.categoryName;
     if (currentCategory !== this.state.currentCategory && !this.state.loading) {
-      this.setState({ loading: true });
+      this.setState({ loading: true, error: false });
       try {
         const data = await apiService.getMoviesByCategory(currentCategory);
         this.setState({
           movies: data.results,
           currentCategory,
           loading: false,
+          error: false,
         });
       } catch {
-        this.setState({ loading: false });
+        this.setState({
+          loading: false,
+          currentCategory,
+          error: 'Fail fetching movies',
+        });
       }
     }
   }
@@ -34,15 +46,29 @@ class MoviesCategory extends React.Component {
     // console.log(this.props.match.params);
 
     return (
-      <div className='MoviesCategory'>
+      <Wrapper className='MoviesCategory'>
+        {this.state.error && (
+          <Alert
+            style={{ maxWidth: '300px' }}
+            message={this.state.error}
+            type='error'
+            showIcon
+          />
+        )}
         {this.state.loading ? (
-          <div style={{display:'flex', justifyContent:'center',padding:'2em'}}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              padding: '2em',
+            }}
+          >
             <Spin size='large' />
           </div>
         ) : (
           <pre>{JSON.stringify(this.state.movies, null, 2)}</pre>
         )}
-      </div>
+      </Wrapper>
     );
   }
 }

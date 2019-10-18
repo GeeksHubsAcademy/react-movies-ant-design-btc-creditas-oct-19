4de398074c4e35e71644ac17e3121a78fbb2b868
jupegarnica <jupegarnica@gmail.com>
2019-10-18 14:39:22 +0200

    deploy

---
+++ docs/precache-manifest.14cf1b7d491f081fd36cca1fa25e6414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2179ad3055cc4b5bce312ae8effaeb03",
+    "url": "/react-movies-ant-design-btc-creditas-oct-19/index.html"
+  },
+  {
+    "revision": "118dc8fd27ef83a95fb5",
+    "url": "/react-movies-ant-design-btc-creditas-oct-19/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "6a7759a1b5ab78741bbe",
+    "url": "/react-movies-ant-design-btc-creditas-oct-19/static/css/main.0e324354.chunk.css"
+  },
+  {
+    "revision": "118dc8fd27ef83a95fb5",
+    "url": "/react-movies-ant-design-btc-creditas-oct-19/static/js/2.3ae76d08.chunk.js"
+  },
+  {
+    "revision": "6a7759a1b5ab78741bbe",
+    "url": "/react-movies-ant-design-btc-creditas-oct-19/static/js/main.5664b556.chunk.js"
+  },
+  {
+    "revision": "8a9169e2ef33c369eb0f",
+    "url": "/react-movies-ant-design-btc-creditas-oct-19/static/js/runtime-main.3ef56056.js"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-movies-ant-design-btc-creditas-oct-19/precache-manifest.14cf1b7d491f081fd36cca1fa25e6414.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movies-ant-design-btc-creditas-oct-19/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});

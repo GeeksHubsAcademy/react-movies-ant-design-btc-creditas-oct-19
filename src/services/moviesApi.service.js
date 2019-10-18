c28cafd41b70af42014b1540df62c2d4b5dc6e27
jupegarnica <jupegarnica@gmail.com>
2019-10-18 10:44:01 +0200

    api service created as singleton

---
+++ src/services/moviesApi.service.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+class ApiService {
+  constructor() {
+      this.APIKEY = '323112ea2281b9eb70f319f4df422c6b';
+      this.callsDone = 0;
+  }
+
+  async getMoviesByCatergory(category, page = 1) {
+    this.callsDone++;
+    return await axios.get(
+      `https://api.themoviedb.org/3/movie/${category}?api_key=${this.APIKEY}&language=es-ES&page=${page}`,
+    );
+  }
+
+  howManyCalls() {
+      return this.callsDone;
+  }
+}
+
+
+export default new ApiService();
